2e44690c01abebaa40318f7857cb03f34dc19072
PPPoE15 <art.viktorov12@gmail.com>
2023-10-26 00:07:41 +0300

    work on home_work_6 SMTU

--- 1 Семестр/СМТУ/home_work_6/Lorenz.m
+++ 1 Семестр/СМТУ/home_work_6/Lorenz.m
@@ -1,13 +1,13 @@
 clc, clear, close all
-x_0 = 20;
+x_0 = 10;
 y_0 = 10;
 z_0 = 10;
-T = 5;
+T = 100;
 s = 10;
 b = 8/3;
-r = 500;
+r = 20;
 
-sys = @(t,x) [s*( x(2) - x(1) ); r*x(1) - x(2) - x(1)*x(2); x(1)*x(2) - b*x(3)];
+sys = @(t,x) [s*(x(2) - x(1)); x(1)*(r-x(3)) - x(2); x(1)*x(2) - b*x(3)];
 [t, sol] = ode45(sys, [0 T], [x_0 y_0 z_0]);
 figure(1)
 plot3(sol(:,1),sol(:,2),sol(:,3))
