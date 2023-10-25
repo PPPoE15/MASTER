975dbe79677cc289f946980b032d9e827c97e321
PPPoE15 <art.viktorov12@gmail.com>
2023-10-25 19:35:33 +0300

    work on lab_4 MMOSU

--- 1 Семестр/ММОСУ/lab_4/practice_4.m
+++ 1 Семестр/ММОСУ/lab_4/practice_4.m
@@ -40,7 +40,7 @@ sys_ob = ss(Ao, Bo, Co, Do)
 % regulator
 sys_reg = ss(k)
 
-sys = lft(sys_ob,sys_reg);
+sys = lft(sys_ob,sys_reg, 1, 4);
 
 C_sys = [0 0 1 0];
 D_sys = 0;
@@ -49,6 +49,7 @@ B_sys = [  0;
            0;
            -k3];
 set(sys, 'C', C_sys, 'D', D_sys, 'B', B_sys);
+sys
 
 %% modeling
 figure(1)
@@ -67,9 +68,9 @@ s2 = -0.05;
 s3 = -0.25-0.08i;
 s4 = -0.25+0.08i;
 p = [s1 s2 s3 s4];
-[K,prec,message] = place(get(sys, 'A'), get(sys, 'B'), p);
+K = acker(get(sys, 'A'), get(sys, 'B'), p);
 sys_reg = ss(K);
-sys = lft(sys_ob,sys_reg);  % WARNING using lft is wrong, check it
+sys = lft(sys_ob,sys_reg, 1, 4);  % WARNING using lft is wrong, check it
 
 C_sys = [0 0 1 0];
 D_sys = 0;
