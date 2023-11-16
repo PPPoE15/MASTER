48e9db106ef373aed474979ab674f80a9fc6921f
PPPoE15 <art.viktorov12@gmail.com>
2023-11-16 11:06:18 +0300

    edit lab 6 MMOSU

--- 1 Семестр/ММОСУ/lab_6/lab_6.m
+++ 1 Семестр/ММОСУ/lab_6/lab_6.m
@@ -1,7 +1,7 @@
 clc, clear, close all
-x_0 = 10;
-y_0 = -10;
-z_0 = 10;
+x_0 = 0.72;
+y_0 = -4.02;
+z_0 = -1.41;
 T = 800;
 b = 0.19;
 
@@ -12,7 +12,7 @@ sys = @(t,x) [  -b*x(1) + sin(x(2));
 
 [t, sol] = ode45(sys, [0 T], [x_0 y_0 z_0]);
 
-D = frac_dim(sol, 0.0004, 2800)
+D = frac_dim(sol, 0.00074, 2821)
 
 figure(1)
 plot3(sol(:,1),sol(:,2),sol(:,3))
