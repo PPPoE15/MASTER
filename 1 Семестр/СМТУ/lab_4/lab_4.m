9b289adc8f97b38344c2ce0f5de81ac38c144670
PPPoE15 <art.viktorov12@gmail.com>
2023-11-26 14:15:54 +0300

    edit lab 4 SMTU

--- 1 Семестр/СМТУ/lab_4/lab_4.m
+++ 1 Семестр/СМТУ/lab_4/lab_4.m
@@ -1,24 +1,59 @@
 clc, clear, close all
 syms a b x
 
-%f = a*x - x^3
-%f = (a-1)*x-x^2
+f = a*x - x^3
+% f = (a-1)*x-x^2
 % f = x^2 + 0.25*a - 1
-f = x^8 + a*x^4 + b
+% f = a*sin(x) + b*sin(x)^2;  %  periodic function
+% f = a*x^2 - 2*(a+1)*x + 3*a - 1
 
-figure(1)
-hold on
-for i = 1:0.5:5
-    g = subs(f, a, i);
-    for k = 1:0.5:5
-        g = subs(g, b, k);
-        [coord, type] = special_points(g, x);
-        for j = 1:size(type, 1)
-            if(type(j,1) == "unstable")
-                plot3(i,k, coord(j,1), 'b.')
-            else
-                plot3(i,k, coord(j,1), 'r.')
+A = [-2, 5]; % range a
+nA = 20; % num a points
+B = [-2, 5]; % range b
+nB = 7; % num b points
+mode = 1; % 1 - one parameter, 2 - two parameters
+
+switch mode
+    case(1)
+        figure(1)
+        hold on
+        for i = linspace(A(1,1),A(1,2),nA)
+            g = subs(f, a, i);
+            [coord, type] = special_points(g, x);
+            for j = 1:size(type, 1)
+                if(type(j,1) == "unstable")
+                    plot(i, coord(j,1), 'b.', 'DisplayName','unstable')
+                else
+                    plot(i, coord(j,1), 'r.', 'DisplayName','stable')
+                end
             end
         end
-    end
+        xlabel('a')
+        ylabel('f')
+        grid on
+        hold off
+
+    case(2)
+        figure(1)
+        hold on
+        for i = linspace(A(1,1),A(1,2),nA)
+            g = subs(f, a, i);
+            for k = linspace(B(1,1),B(1,2),nB)
+                g = subs(g, b, k);
+                [coord, type] = special_points(g, x);
+                for j = 1:size(type, 1)
+                    if(type(j,1) == "unstable")
+                        plot3(i,k, coord(j,1), 'b.')
+                    else
+                        plot3(i,k, coord(j,1), 'r.')
+                    end
+                end
+            end
+        end
+        xlabel('a')
+        ylabel('b')
+        zlabel('f')
+        grid on
+        hold off
 end
+
