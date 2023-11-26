24c03f393f43c58bc24fb70edf42c265dc1a068f
PPPoE15 <art.viktorov12@gmail.com>
2023-11-26 16:29:33 +0300

    edit lab 4 SMTU

--- 1 Семестр/СМТУ/lab_4/lab_4.m
+++ 1 Семестр/СМТУ/lab_4/lab_4.m
@@ -1,17 +1,17 @@
 clc, clear, close all
 syms a b x
 
-f = a*x - x^3
+% f = a*x - x^3
 % f = (a-1)*x-x^2
 % f = x^2 + 0.25*a - 1
-% f = a*sin(x) + b*sin(x)^2;  %  periodic function
+f = a*sin(x) + b*sin(x)^2;  %  periodic function
 % f = a*x^2 - 2*(a+1)*x + 3*a - 1
 
-A = [-2, 5]; % range a
-nA = 20; % num a points
-B = [-2, 5]; % range b
-nB = 7; % num b points
-mode = 1; % 1 - one parameter, 2 - two parameters
+A = [-3, 3]; % range a
+nA = 10; % num a points
+B = [-3, 3]; % range b
+nB = 10; % num b points
+mode = 2; % 1 - one parameter, 2 - two parameters
 
 switch mode
     case(1)
@@ -22,9 +22,9 @@ switch mode
             [coord, type] = special_points(g, x);
             for j = 1:size(type, 1)
                 if(type(j,1) == "unstable")
-                    plot(i, coord(j,1), 'b.', 'DisplayName','unstable')
+                    plot(i, coord(j,1), 'b.')
                 else
-                    plot(i, coord(j,1), 'r.', 'DisplayName','stable')
+                    plot(i, coord(j,1), 'r.')
                 end
             end
         end
