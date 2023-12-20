3bf04cad0862cbc64591c0e5ff137e11de15afa2
PPPoE15 <art.viktorov12@gmail.com>
2023-12-20 19:11:06 +0300

    working on idz SMTU

--- 1 Семестр/СМТУ/IDZ/Hardy.m
+++ 1 Семестр/СМТУ/IDZ/Hardy.m
@@ -1,7 +1,7 @@
 clc, clear
 syms w s
-W = (6*s + 10) / (s+5) / (s^2 + 4*s + 30)
+W = (6*s + 10) / (s+5) / (s^2 + 4*s + 30);
 W1 = subs(W, s, 1i*w);
 W2 = subs(W, s, -1i*w);
 W_abs = simplify(W1*W2)
-
+pretty(W_abs)
