d07b0337b1a556c0c12ebab7ee15d3b829e28609
PPPoE15 <art.viktorov12@gmail.com>
2024-02-23 21:51:04 +0300

    work on lab_1 POSU

--- 2_sem/POSU/1_lab/lab_1_script.m
+++ 2_sem/POSU/1_lab/lab_1_script.m
@@ -1,19 +1,24 @@
 clc, clear, close all
-syms x y z
-
-x = [x y z];
-
-x0 = [2 3 4];
-sys = @ (t,x) [x(2); 0.5*x(3); -2*x(1) - 4*x(2) - 3*x(3)];
-[t, y] = ode45(sys,[0 5],x0);
-
-A = [0  1  0;
-     0  0  0.5;
-    -2 -4 -3];
+x_0 = 2;
+y_0 = 0;
+z_0 = 0;
+T = 20;
 
 
-plot(t, y(1), '.b',t, y(2), '.r',t, y(3), '.k')
+
+sys = @(t,x) [  x(2); 
+                0.5*x(3); 
+                -2*x(1) - 4*x(2) - 3*x(3)];
+
+[t, sol] = ode45(sys, [0 T], [x_0 y_0 z_0]);
 
 
-S = dsolve('Dx1=x2','Dx2=0.5*x3',' Dx3=-2*x1-4*x2-3*x3', ...
-    'x1(0)=2','x2(0)=0','x3(0)=0')
+figure(1)
+plot3(sol(:,1),sol(:,2),sol(:,3))
+grid on
+figure(2)
+plot(t,sol(:,1),t,sol(:,2),t,sol(:,3))
+grid on
+
+
+
