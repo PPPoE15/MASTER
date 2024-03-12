728d2d61c3ad0fb1afc081b4e5860473ecd99b32
PPPoE15 <art.viktorov12@gmail.com>
2024-03-12 10:19:32 +0300

    finaly a got lab_1 POSU

--- 2_sem/POSU/1_lab/lab_1_script.m
+++ 2_sem/POSU/1_lab/lab_1_script.m
@@ -15,47 +15,40 @@ figure(1)
 plot3(sol(:,1),sol(:,2),sol(:,3))
 grid on
 figure(2)
-plot(t,sol(:,1),t,sol(:,2),t,sol(:,3))
+title('Численное решение');
+hold on
+plot(t,sol(:,1), '.',t,sol(:,2), '.',t,sol(:,3), '.')
 grid on
 
-syms x1 x2 x3 dx1 dx2 dx3 s
 
 
-A = [0 1 0;
-     0 0 0.5;
-     -2 -4 -3]
-% dx1 = x2;
-% dx2 = 0.5*x3;
-% dx3 = -2*x1 -4*x2 -3*x3;
-% s*x1 -x2 = 2
-% s*x2 -0.5*x3 = 0
-% s*x3 2*x1 + 4*x2 + 3*x3 = 0
+syms x1(t) x2(t) x3(t) a
+eqn(1) = diff(x1,t) == x2;
+eqn(2) = diff(x2,t) == 0.5*x3;
+eqn(3) = diff(x3,t) == -2*x1 - 4*x2 - 3*x3;
+cond(1) = x1(0) == 2;
+cond(2) = x2(0) == 0;
+cond(3) = x3(0) == 0;
+S = dsolve(eqn, cond)
 
-delta1 = [2 -1 0; 
-          0 s -0.5;
-          0 4 s+3];
+syms t z; % Объявляем временную переменную t как символьную
+% Определяем корни полинома
+z1 = root(z^3 + 3*z^2 + 2*z + 1, z, 1);
+z2 = root(z^3 + 3*z^2 + 2*z + 1, z, 2);
+z3 = root(z^3 + 3*z^2 + 2*z + 1, z, 3);
 
-delta2 = [s 2 0; 
-          0 0 -0.5;
-          2 0 s+3];
-
-delta3 = [s -1 2; 
-          0 s 0;
-          2 4 0];
+% Определяем функцию
+f1 = S.x1;
+f2 = S.x2;
+f3 = S.x3;
 
 
-delta = [s -1 0; 
-          0 s -0.5;
-          2 4 s+3];
-
-ddelta = det(delta);
-ddelta1 = det(delta1);
-ddelta2 = det(delta2);
-ddelta3 = det(delta3);
-x1 = ddelta1/ddelta
-x2 = ddelta2/ddelta
-x3 = ddelta3/ddelta
-% t = 0:0.01:20;
-% figure(3)
-% x1 = 2.*exp(-t)+2.*t.*exp(-t);
-% plot(t,x1)
+% Построение графика
+figure(3)
+hold on
+fplot(f1, [0, 20]); 
+fplot(f2, [0, 20]);
+fplot(f3, [0, 20]);
+xlabel('t');
+ylabel('f(t)');
+title('Аналитическое решение');
