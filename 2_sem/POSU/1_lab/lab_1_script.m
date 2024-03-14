9eebd48160db11f6e8540674236182702a0652ac
PPPoE15 <art.viktorov12@gmail.com>
2024-03-14 19:42:32 +0300

    done with lab_1 POSU

--- 2_sem/POSU/1_lab/lab_1_script.m
+++ 2_sem/POSU/1_lab/lab_1_script.m
@@ -2,14 +2,13 @@ clc, clear, close all
 x_0 = 2;
 y_0 = 0;
 z_0 = 0;
-T = 20;
+Tm = 20;
 
 sys = @(t,x) [  x(2); 
                 0.5*x(3); 
                 -2*x(1) - 4*x(2) - 3*x(3)];
 
-[t, sol] = ode45(sys, [0 T], [x_0 y_0 z_0]);
-
+[T, sol] = ode45(sys, [0 Tm], [x_0 y_0 z_0]);
 
 figure(1)
 plot3(sol(:,1),sol(:,2),sol(:,3))
@@ -17,7 +16,7 @@ grid on
 figure(2)
 title('Численное решение');
 hold on
-plot(t,sol(:,1), '.',t,sol(:,2), '.',t,sol(:,3), '.')
+plot(T,sol(:,1), '.',T,sol(:,2), '.',T,sol(:,3), '.')
 grid on
 
 
@@ -36,19 +35,24 @@ syms t z; % Объявляем временную переменную t как 
 z1 = root(z^3 + 3*z^2 + 2*z + 1, z, 1);
 z2 = root(z^3 + 3*z^2 + 2*z + 1, z, 2);
 z3 = root(z^3 + 3*z^2 + 2*z + 1, z, 3);
-
 % Определяем функцию
 f1 = S.x1;
 f2 = S.x2;
 f3 = S.x3;
-
-
 % Построение графика
 figure(3)
 hold on
-fplot(f1, [0, 20]); 
-fplot(f2, [0, 20]);
-fplot(f3, [0, 20]);
+fplot(f1, [0, Tm]); 
+fplot(f2, [0, Tm]);
+fplot(f3, [0, Tm]);
 xlabel('t');
 ylabel('f(t)');
 title('Аналитическое решение');
+
+figure(4)
+hold on
+plot(T,sol(:,1), 'o',T,sol(:,2), 'o',T,sol(:,3), 'o')
+fplot(f1, [0, Tm]); 
+fplot(f2, [0, Tm]);
+fplot(f3, [0, Tm]);
+grid on
